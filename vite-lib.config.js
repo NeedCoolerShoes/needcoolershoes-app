c0519040b8d2dea2820764535913073ac11cac4d
Nucceteere <ruzgar@nucceteere.xyz>
2025-05-31 01:00:13 +0300

    feat: tauri yay

--- vite-lib.config.js
+++ vite-lib.config.js
@@ -3,13 +3,44 @@ import { fileURLToPath } from "node:url";
 import { defineConfig } from "vite";
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
+const host = process.env.TAURI_DEV_HOST;
 
 export default defineConfig({
+  clearScreen: false,
+  server: {
+    port: 1420,
+    // Tauri expects a fixed port, fail if that port is not available
+    strictPort: true,
+    // if the host Tauri is expecting is set, use it
+    host: host || false,
+    hmr: host
+    ? {
+      protocol: 'ws',
+      host,
+      port: 1421,
+    }
+    : undefined,
+
+    watch: {
+      // tell vite to ignore watching `src-tauri`
+      ignored: ['**/src-tauri/**'],
+    },
+  },
+  envPrefix: ['VITE_', 'TAURI_ENV_*'],
   build: {
     lib: {
       entry: resolve(__dirname, "src/main.js"),
       name: "ncrs-editor",
       fileName: "ncrs-editor",
-    }
+    },
+    target:
+      process.env.TAURI_ENV_PLATFORM == 'windows'
+        ? 'chrome105'
+        : 'safari13',
+    // don't minify for debug builds
+    minify: !process.env.TAURI_ENV_DEBUG ? 'esbuild' : false,
+    // produce sourcemaps for debug builds
+    sourcemap: !!process.env.TAURI_ENV_DEBUG,
+   }
   }
 })
